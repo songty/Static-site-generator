04a7d74c8226b6aac53d0c3d9dc101bcb170a033
Tian Song <tian.y.song@gmail.com>
2014-06-03 16:07:45 -0700

    streamlined things and made things easier to use

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ var options = { encoding: 'utf8' };
 var path = require('path');
 
 var fileReader = module.exports.fileReader = function (textFile, cb) {
-	var newPath = path.join(__dirname, textFile);
-	fs.readFile(newPath, options, function(err, contents) {
+	fs.readFile(textFile, options, function(err, contents) {
 		cb(null, contents);
 	});
 };
 
+/**
+ * Reads files at a path
+ *
+ * @function
+ * @param {string} ipath - The path at which to read
+ * @param {function} cb - The callback to call. The callback receives two
+ * arguments, `err`, an error, and `files`, an array of file names.
+ */
 var pathReader = module.exports.pathReader = function (ipath, cb) {
-	var newPath = path.join(__dirname, ipath);
-	fs.readdir(newPath, function(err, files) {
+	fs.readdir(ipath, function(err, files) {
 		cb(null, files);
 	});
 };
 
 //Makes a new file in the generated directory
-var fileWriter = module.exports.fileWriter = function (newFile) {
-	var targetDir = path.join(__dirname, 'generated', newFile);
-	fs.writeFile(targetDir, 'contents', options, function (err) {
+var fileWriter = module.exports.fileWriter = function (newFile, contents) {
+	fs.writeFile(newFile, contents, options, function (err) {
 		if (err) throw err;
   	});
 };
@@ -37,9 +42,29 @@ var fileShreader  = module.exports.fileShreader = function (cb) {
 };
 
 var isHtml = module.exports.isHtml = function(ifile) {
-	if (ifile.search('html') !== -1) {
-		return true;
-	} else {
-		return false;
-	}
+	return ifile.search('html') !== -1;
+};
+
+/**
+ * Reading the contents of the source pages
+ *
+ * @function
+ * @param {function} cb - The callback to call. The callback receives one
+ * argument, `pages`, an array of html page file names.
+ */
+var sourcePage = module.exports.sourcePage = function(path, cb) {
+	pathReader(path, function(err, files) {
+		var filtered = files.filter(isHtml);
+		cb(filtered);
+	});
+};
+
+//Splits the default template into two parts on {{ content }}
+var sourceTemplate = module.exports.sourceTemplate = function(path, cb) {
+	fileReader(path, function(err, contents) {
+		if (contents.search("{{ content }}") !== -1) {
+			var defaultTemplate = contents.split("{{ content }}");
+			cb(defaultTemplate);
+		}
+	});
 };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 var chai = require('chai');
 var expect = chai.expect;
-var _ = require('../index');
+var lib = require('../index');
 var path = require('path');
+var fixtures =  function (fix) {
+	return path.join(__dirname, 'fixtures', fix);
+};
 
 describe('fileReader()', function() {
 	it('should read a file', function(done) {
-		_.fileReader('text.txt', function (err, contents) {
+		lib.fileReader(fixtures('text.txt'), function (err, contents) {
 			expect(contents).to.be.eql('this is just some text');
 			done();
 		});
@@ -14,14 +17,14 @@ describe('fileReader()', function() {
 
 describe('pathReader()', function() {
 	it('shoud turn directory items into a number of arrays', function(done) {
-		_.pathReader('source', function(err, files) {
-			expect(files.length).to.be.eql(3);
+		lib.pathReader(fixtures('source'), function(err, files) {
+			expect(files.length).to.be.eql(2);
 			done();
 		});
 	});
 	it('should return the array', function(done) {
-		_.pathReader('test', function(err, files) {
-			expect(files).to.be.eql(['.DS_Store', 'test.js', 'text.txt']);
+		lib.pathReader('test', function(err, files) {
+			expect(files).to.be.eql(['fixtures', 'test.js']);
 			done();
 		});
 	});
@@ -29,16 +32,41 @@ describe('pathReader()', function() {
 
 describe('isHtml()', function() {
 	it('should return false for non-html files', function () {
-		expect(_.isHtml('blablab.hml')).to.be.eql(false);
+		expect(lib.isHtml('blablab.hml')).to.be.eql(false);
 	});
 	it('should return true for html files', function() {
-		expect(_.isHtml('blah.html')).to.be.eql(true);
+		expect(lib.isHtml('blah.html')).to.be.eql(true);
+	});
+});
+
+describe('sourceTemplate()', function() {
+	it('should return an array with two items from template', function(done) {
+		lib.sourceTemplate(fixtures('source/layouts/testme!.html'), function(array) {
+			expect(array).to.be.eql(['testme!\n', '\ntest!']);
+			done();
+		});
+	});
+});
+
+describe('sourcePage()', function() {
+	it('returns array of pages that needs be beautified', function(done) {
+		lib.sourcePage(fixtures('source/pages'), function(pages) {
+			expect(pages).to.be.eql(['about.html', 'home.html']);
+			done();
+		});
+	});
+});
+
+
+describe('fileWriter()', function() {
+	it.skip('creates a new file in generated directory', function(done) {
+		lib.fileWriter('testcase');
 	});
 });
 
 describe('fileShreader()', function() {
 	it.skip('deletes files inside a direcotry', function(done) {
-		_.fileShreader(function(err, files) {
+		lib.fileShreader(function(err, files) {
 			expect();
 		});
 	});
